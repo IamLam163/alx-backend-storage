f0236580dcf352df3e3c018ffb2cddbdff260c3f
IamLam163 <olamidealiyu@gmail.com>
2023-04-08 18:20:02 +0100

    the year difference is 30 years. split by 30

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -9,16 +9,19 @@ FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
 */
+
 /*
-SELECT band_name, (IFNULL(split, '2000') - formed) AS lifespan
+SELECT band_name,
+(IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
     ORDER BY lifespan DESC;
-    */
 
+*/
 SELECT band_name,
-(IFNULL(split, YEAR(NOW())) - formed) AS lifespan
+(IFNULL(split, '2020') - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 GROUP BY band_name, split, formed
 ORDER BY lifespan DESC;
+
