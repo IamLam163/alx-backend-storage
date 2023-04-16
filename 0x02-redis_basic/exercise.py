35986fdbdc61166fe8be7444296101b630b882f9
IamLam163 <olamidealiyu@gmail.com>
2023-04-16 19:00:27 +0100

    getting original data type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,7 +8,7 @@ the random key and return the key.
 """
 
 import redis
-from typing import Union
+from typing import Callable, Optional, Union
 from uuid import uuid4
 
 class Cache:
@@ -22,3 +22,39 @@ class Cache:
         rdmkey = str(uuid4())
         self._redis.set(rdmkey, data)
         return rdmkey
+
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], Union[str, bytes, int, float]]] = None) -> Union[str, bytes, int, float, None]:
+            """get key and return data is fn is None"""
+            data = self._redis.get(key)
+            if data is None:
+                    return None
+            if fn is not None:
+                    return fn(data)
+            else:
+                    return data
+
+    def get_str(self, key: str) -> Union[str, bytes, int, float, None]:
+             """automatically parametrize Cache.get with the correct conversion function"""
+             data = self.get(key, lambda d: d.decode('utf-8'))
+             return data
+
+
+    def get_int(self, key: str) -> Union[str, bytes, int, float, None]:
+             """automatically parametrize Cache.get with the correct conversion function"""
+             # data = self.get(key, int)
+             data = self.get(key, lambda d: int(d))
+             return data
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
